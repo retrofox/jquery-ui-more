2207c9f265c8337eea73cfbfc576743e950ad151
Damian Suarez <damian.suarez@xifox.net>
2011-04-28 01:56:59 +0000

    auto handle height

--- sources/jquery-ui/ui/jquery.ui.scrollbar.js
+++ sources/jquery-ui/ui/jquery.ui.scrollbar.js
@@ -16,44 +16,52 @@
         // alias
         var _o = this.options
           , cp = _o.classPrefix
+          , _sd = _o.sides
           ;
         // determine which bar will be added
         this.add = {
-          x: _o.sides == 'both' || _o.sides == 'x' ? true : false,
-          y: _o.sides == 'both' || _o.sides == 'y' ? true : false
+          x: _sd == 'both' || _o.sides == 'x' ? true : false,
+          y: _sd == 'both' || _o.sides == 'y' ? true : false
         }
 
-        console.debug('this.add -> ', this.add);
-
-
-        // * scrollbar elements *
+        // dims to calculate
+        var _d = this.dims = {x: {}, y: {}};
 
         // ** scrollbar-placeholder **
         if(!this.element.children(cp+'placeholder').length)
           this.element.wrapInner($('<div class="' + cp + 'placeholder" />'));
 
+        _d.x.h = this.element.width();
+        _d.y.h = this.element.height();
+
         this.placeholder = this.element.find('.'+cp+'placeholder')
           .css('position', 'relative')
-          .height(this.element.height() || this.options.height);
+          .height(_d.y.h);
        
         // ** scrollbar-wrapper
         if(!this.placeholder.find(cp+'wrapper').length)
           this.placeholder.wrapInner($('<div class="' + cp + 'wrapper" />'));
-
+        
         this.wrapper = this.placeholder.find('.'+cp+'wrapper')
-          //.css('overflow', 'hidden')
-          .css('overflow', 'auto')
-          .outerHeight(this.placeholder.outerHeight());
+          .css('overflow', 'hidden')
+          .outerHeight(_d.y.h);
 
         // ** scrollbar-container **
         if(!this.wrapper.find(cp+'container').length)
           this.wrapper.wrapInner($('<div class="' + cp + 'container" />'));
         this.container = this.wrapper.find('.'+cp+'container');
 
+        _d.x.c = this.container.outerWidth();
+        _d.y.c = this.container.outerHeight();
+
+        // percentage
+        _d.y.p = _d.y.h/_d.y.c;
+
         // offset
         this.offset = {
-          y: this.container.outerHeight() - this.wrapper.outerHeight()
+          y: _d.y.c - _d.y.h
         }
+
         // add Scrollbar
         this._addScrollbar();
       }
@@ -62,7 +70,7 @@
         var self = this;
 
         // * Y scrollbar *
-        if(this.offset.y > 0) {
+        if(this.add.y && this.offset.y > 0) {
 
           // add slider
           if(!this.wrapper.find('.ui-slider-vertical').length)
@@ -85,25 +93,30 @@
             }
           });
 
-          // recalculation of position
-          var handle = this.slider.y.find('.ui-slider-handle')  // slide handle element
-            , hH = handle.outerHeight()
-            , mH = Math.round(hH/2)
-            , hS = this.placeholder.outerHeight() - parseInt(this.slider.y.css('margin-top'))*2 - hH
+          // slide handle element
+          var handle = this.slider.y.find('.ui-slider-handle');
 
-          // esislider size/position
+          // set handle height
+          handle.outerHeight(this.dims.y.h*this.dims.y.p);
+          
+          // re-calculate positions/dims
+          var hH = handle.outerHeight()
+            , hmH = Math.round(hH/2)
+            , hS = this.dims.y.h - parseInt(this.slider.y.css('margin-top'))*2 - hH
+
+          // slider size/position
           this.slider.y.css({
             height: hS,
             top: hH
           });
 
-          // 
+          // handle slide/position 
           handle.css({
             left: -Math.round(handle.outerWidth()/2),
-            marginTop: -Math.round(mH)
+            marginTop: -Math.round(hmH)
           });
 
-        // mousewheel to placeholder
+        // add mousewheel event
         this.placeholder.mousewheel(function(ev, d, x, y){
           var vs = self.slider.y.slider('value');
           self.slider.y.slider('value', vs+d*self.step.y);
